454200a2a6b76c0985b94be607c95889c8b6986b
Soseh <sosehv@gmail.com>
2024-12-20 15:44:44 -0500

    Adds two more tests about input types

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -41,12 +41,23 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return false if the array elements are not strings", function() {
+  it("should return false if the first argument is not an array", function() {
+    const result = wordSearch('This is not an array', 'SEND');
+    assert.isFalse(result);
+  });
+
+  it("should return false if the second argument is not a string", function() {
     const result = wordSearch([
-      [1, 2, 3, 4, 5, 6],
-      [7, 8, 1, 2, 3, 4],
-      [5, 6, 7, 8, 1, 2],
-     ], 'SEND');
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 12);
     assert.isFalse(result);
   });
 });
